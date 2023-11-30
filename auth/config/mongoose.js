2c63e8ad434514761ac9229037eb67c3ad87142f
Utkarsh Singh <singhrishu680@gmail.com>
2023-11-30 13:09:32 +0530

    Untrack files in .gitignore

--- auth/config/mongoose.js
+++ auth/config/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 // mongoose.connect('mongodb://127.0.0.1/authentication')
 
 mongoose.connect(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.taxsifz.mongodb.net/?retryWrites=true&w=majority`);
-
+// hello
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'error conneting to MongoDB'))
 
